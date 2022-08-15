24a81cad8b1d34ee3eef71a417013a441776549a
hryen <henrychenchn@gmail.com>
2022-08-15 22:37:04 +0800

    移除 watchexec

--- myPhotos.go
+++ myPhotos.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"myPhotos/config"
 	"myPhotos/scanner"
-	"myPhotos/third_party/watchexec"
 	"myPhotos/web"
 	"os"
 )
@@ -22,17 +21,12 @@ func main() {
 			config.GPSToGeo = true
 		}
 		scanner.StartScan(os.Args[2])
-	} else if os.Args[1] == "watchexec" {
-		watchexec.WatchExec()
-	} else if os.Args[1] == "watch" {
-		watchexec.StartWatchExec(os.Args[2])
 	}
 }
 
 func printUsage() {
 	fmt.Println("USAGE:")
-	fmt.Println("  homePhoto server              start web server")
-	fmt.Println("  homePhoto scan DIR -g         scan dir and save to database")
-	fmt.Println("                                -g: get geography info(use AMap api)")
-	fmt.Println("  homePhoto watch DIRS...       watch dirs file change, dirs use ; separate")
+	fmt.Println("  myPhotos server              start web server")
+	fmt.Println("  myPhotos scan DIR [-g]         scan dir and save to database")
+	fmt.Println("                               -g: get geography info(use AMap api)")
 }
