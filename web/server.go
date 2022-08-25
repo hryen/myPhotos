670d103cfd6c1776c28ced6fe95a0f59f330aafc
hryen <henrychenchn@gmail.com>
2022-08-26 00:32:16 +0800

    remove frontend

--- web/server.go
+++ web/server.go
@@ -1,18 +1,13 @@
 package web
 
 import (
-	"embed"
 	"github.com/gorilla/mux"
-	"io/fs"
 	"myPhotos/api"
 	"myPhotos/config"
 	"myPhotos/logger"
 	"net/http"
 	"os"
 )
-
-//go:embed static
-var static embed.FS
 
 func StartServer() {
 	r := mux.NewRouter()
@@ -25,9 +20,7 @@ func StartServer() {
 	r.HandleFunc("/api/medias/{id}/video", api.GetLivePhotoVideo).Methods(http.MethodGet)
 	r.HandleFunc("/api/medias/{id}/info", api.GetMediaInfo).Methods(http.MethodGet)
 
-	// frontend
-	staticFS, _ := fs.Sub(static, "static")
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.FS(staticFS))))
+	r.Use(mux.CORSMethodMiddleware(r))
 
 	logger.InfoLogger.Println("About to listen on " + config.HTTPPort + ", Go to http://" + config.HTTPAddr)
 	err := http.ListenAndServe(config.HTTPAddr+":"+config.HTTPPort, r)
