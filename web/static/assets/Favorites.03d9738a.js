e067d8cbf30a1a97f782c71bf6f344bb199d6830
hryen <henrychenchn@gmail.com>
2023-04-06 14:10:13 +0800

    更新前端

---
+++ web/static/assets/Favorites.03d9738a.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.25ab788f.js";const e={};function t(_,a){return" favorites "}var o=r(e,[["render",t]]);export{o as default};
--- web/static/assets/Favorites.5a02000a.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.5b613125.js";const e={};function t(_,a){return" favorites "}var o=r(e,[["render",t]]);export{o as default};
